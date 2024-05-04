557bf75c228f5bf6b928317f36dc55180c636a79
Hugh Anderson <hughabcanderson@gmail.com>
2024-05-04 16:02:40 -0400

    flow control - an equivalent while loop

--- AutomateTheBoringStuffWithPython.py
+++ AutomateTheBoringStuffWithPython.py
@@ -1,4 +1,6 @@
-total = 0
-for num in range(101):
-    total = total + num
-print(total)
+# an equivalent while loop
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times ('+ str(i) + ')')
+    i = i + 1
